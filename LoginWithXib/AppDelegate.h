2017f32104df5e99df2f800cabc501a7a3b19fd2
BHAVIK <panchal.bhavik0919@gmail.com>
2016-05-29 10:10:20 +0530

    Initial Commit

---
+++ LoginWithXib/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoginWithXib
+//
+//  Created by Bhavik Panchal on 5/29/16.
+//  Copyright © 2016 Bhavik Panchal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoginWithXib/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoginWithXib
+//
+//  Created by Bhavik Panchal on 5/29/16.
+//  Copyright © 2016 Bhavik Panchal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
